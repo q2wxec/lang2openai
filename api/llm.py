7b15991e140b0f8d9ed5716de473d7f866fd0682
fxx <q2wxec@qq.com>
2024-04-30 16:16:40 +0800

    feat: 保留基于提示词的function call适配

--- api/llm.py
+++ api/llm.py
@@ -2,6 +2,7 @@ from sanic.response import json as sanic_json
 from sanic.response import ResponseStream
 from sanic import request
 import json
+import re
 
 import asyncio
 import os
@@ -36,15 +37,10 @@ async def chat(req: request):
     temperature = safe_get(req, 'temperature', 0.01)
     #print('messages:'+str(messages))
     #print('tools:'+str(tools))
-    
     # spark-3.1最新消息必须来自用户
     if model == "spark-3.1" and messages[-1]['role'] != 'user' :
         messages[-1]['role'] = 'user'
     chat : BaseChatModel = getChat(model,temperature)
-    # tools不为空，说明是工具调用
-    is_function_call = (tools and messages[-1]['role'] == 'user')
-    # if is_function_call:
-    #     return function_call(req,chat)
     chat_messages = []
     for message in messages:
         if message['content'] is None:
@@ -67,7 +63,17 @@ async def chat(req: request):
             # chat_message = ToolMessage(content=message['content'],tool_call_id = message['tool_calls'])
             chat_message = AIMessage(content='工具调用结果如下，tool_call_id：'+message['tool_call_id']+' ,调用结果result:'+message['content'])
         chat_messages.append(chat_message)
-    
+    # tools不为空，说明是工具调用
+    is_function_call = (tools and messages[-1]['role'] == 'user')
+    if is_function_call:
+        stream = False
+        question = chat_messages.pop().content
+        # 将tools转化为str
+        functions = json.dumps(tools)
+        function_prompt = get_function_prompt(question,functions)
+        chat_message = HumanMessage(content=function_prompt)
+        chat_messages.append(chat_message)
+        
     prompt_tokens = sum(cal_tokens(s['content'], 'gpt-3.5-turbo') for s in messages)
     if stream:
         stream_resp = get_chat_stream_resp(model)
@@ -97,7 +103,26 @@ async def chat(req: request):
         resp["usage"]["prompt_tokens"]= prompt_tokens
         resp["usage"]["completion_tokens"]= completion_tokens
         resp["usage"]["total_tokens"]= completion_tokens+prompt_tokens
-        resp["choices"][0]['message']['content'] = content 
+        if is_function_call:
+            tool_calls = []
+            if is_valid_json_array(content):
+                tool_array = json.loads(content)
+            else:
+                match = re.search(r'\[.*\]', content)
+                if match:
+                    tool_array = json.loads(match.group(0))
+                else:
+                    raise Exception('函数调用结果格式错误，请检查')
+            for tool in tool_array:
+                tool_resp = {
+                        "id": uuid.uuid4().hex,
+                        "type": "function",
+                        "function": tool
+                    }
+                tool_calls.append(tool_resp)
+            resp["choices"][0]['message']['tool_calls'] = tool_calls
+        else:
+            resp["choices"][0]['message']['content'] = content 
         #print('resp:'+str(resp))
         return sanic_json(resp)
 
